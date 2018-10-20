6a582c3e58991dbdde6edeb87ccefae6df83ec1a
Eli Heuer <eliheuer@gmail.com>
2018-10-20 00:17:03 -0400

    Hello World

---
+++ hw.py
@@ -0,0 +1,15 @@
+
+import random
+
+
+Print("************************")
+Print("*                      *")
+Print("*   The Hidden Words   *")
+Print("*       From the       *")
+Print("*   Arabic & Persian   *")
+Print("*                      *")
+Print("************************")
+
+
+
+
